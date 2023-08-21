d5ae5edc7dda5ec41b95e1da95b13843e1445560
Paweł Kruszko <pawelkruszko@gmail.com>
2023-08-21 09:03:30 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from sklearn.naive_bayes import *
+from sklearn.dummy import *
+from sklearn.ensemble import *
+from sklearn.neighbors import *
+from sklearn.tree import *
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.calibration import *
+from sklearn.linear_model import *
+from sklearn.multiclass import *
+from sklearn.svm import *
+import pandas
